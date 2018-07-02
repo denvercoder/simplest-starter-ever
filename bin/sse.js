c8acad36ee7ddeee3837e7b76bb78c98e6ec44ba
Tim Myers <tim@denvercoder.com>
2018-07-01 23:04:35 -0600

    fixed file name conflict

--- bin/sse.js
+++ bin/sse.js
@@ -8,30 +8,7 @@ const { exec } = require('child_process')
 const packageJson = require('../package.json')
 
 const scripts = `"start": "cross-env NODE_ENV=development webpack-dev-server -d",
-    "build": "cross-env NODE_ENV=production webpack -p",
-    "test": "jest"`
-
-const jestConfig = `"license": "ISC",
-  "jest": {
-    "moduleFileExtensions": [
-      "js",
-      "jsx"
-    ],
-    "moduleDirectories": [
-      "node_modules"
-    ],
-    "setupFiles": [
-      "<rootDir>/src/tests/setup.js"
-    ],
-    "moduleNameMapper": {
-      "\\\\.(css|styl|less|sass|scss)$": "identity-obj-proxy"
-    },
-    "transform": {
-      "^.+\\\\.js$": "babel-jest",
-      "^.+\\\\.jsx$": "babel-jest",
-      "\\\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/tests/__mock__/fileTransformer.js"
-    }
-  }`
+    "build": "cross-env NODE_ENV=production webpack -p"`
 
 /**
  * we pass the object key dependency || devdependency to this function
@@ -64,10 +41,7 @@ exec(
 		// replace the default scripts, with the webpack scripts in package.json
 		fs.readFile(packageJSON, (err, file) => {
 			if (err) throw err
-			const data = file
-				.toString()
-				.replace('"test": "echo \\"Error: no test specified\\" && exit 1"', scripts)
-				.replace('"license": "ISC"', jestConfig)
+			const data = file.toString()
 			fs.writeFile(packageJSON, data, err2 => err2 || true)
 		})
 
