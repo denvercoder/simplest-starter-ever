d5f1ad7f11455da131e66bfb65f08852b189de30
Tim Myers <tim@denvercoder.com>
2018-07-01 23:46:05 -0600

    edits to sse.js

--- bin/sse.js
+++ bin/sse.js
@@ -1,14 +1,14 @@
 #!/usr/bin/env node
 
-const fs = require('fs-extra')
-const path = require('path')
-const https = require('https')
-const { exec } = require('child_process')
+const fs = require('fs-extra');
+const path = require('path');
+const https = require('https');
+const { exec } = require('child_process');
 
-const packageJson = require('../package.json')
+const packageJson = require('../package.json');
 
 const scripts = `"start": "cross-env NODE_ENV=development webpack-dev-server -d",
-    "build": "cross-env NODE_ENV=production webpack -p"`
+    "build": "cross-env NODE_ENV=production webpack -p"`;
 
 /**
  * we pass the object key dependency || devdependency to this function
@@ -24,9 +24,9 @@ const getDeps = deps =>
 		.replace(/,/g, ' ')
 		.replace(/^/g, '')
 		// exclude the plugin only used in this file, nor relevant to the boilerplate
-		.replace(/fs-extra[^\s]+/g, '')
+		.replace(/fs-extra[^\s]+/g, '');
 
-console.log('Initializing project..')
+console.log('Initializing project..');
 
 // create folder and initialize npm
 exec(
@@ -34,70 +34,73 @@ exec(
 	(initErr, initStdout, initStderr) => {
 		if (initErr) {
 			console.error(`Everything was fine, then it wasn't:
-    ${initErr}`)
-			return
+    ${initErr}`);
+			return;
 		}
-		const packageJSON = `${process.argv[2]}/package.json`
+		const packageJSON = `${process.argv[2]}/package.json`;
 		// replace the default scripts, with the webpack scripts in package.json
 		fs.readFile(packageJSON, (err, file) => {
-			if (err) throw err
-			const data = file.toString()
-			fs.writeFile(packageJSON, data, err2 => err2 || true)
-		})
+			if (err) throw err;
+			const data = file
+				.toString()
+				.replace('"test": "echo \\"Error: no test specified\\" && exit 1"', scripts)
+				.replace('"license": "ISC"', jestConfig);
+			fs.writeFile(packageJSON, data, err2 => err2 || true);
+		});
 
-		const filesToCopy = ['README.md', 'webpack.config.js']
+		const filesToCopy = ['README.md', 'webpack.config.js'];
 
 		for (let i = 0; i < filesToCopy.length; i += 1) {
 			fs.createReadStream(path.join(__dirname, `../${filesToCopy[i]}`)).pipe(
-				fs.createWriteStream(`${process.argv[2]}/${filesToCopy[i]}`),
-			)
+				fs.createWriteStream(`${process.argv[2]}/${filesToCopy[i]}`)
+			);
 		}
 
 		https.get(
 			'https://raw.githubusercontent.com/denvercoder/simplest-starter-ever/master/.gitignore',
 			res => {
-				res.setEncoding('utf8')
-				let body = ''
+				res.setEncoding('utf8');
+				let body = '';
 				res.on('data', data => {
-					body += data
-				})
+					body += data;
+				});
 				res.on('end', () => {
 					fs.writeFile(`${process.argv[2]}/.gitignore`, body, { encoding: 'utf-8' }, err => {
-						if (err) throw err
-					})
-				})
-			},
-		)
+						if (err) throw err;
+					});
+				});
+			}
+		);
 
-		console.log('npm init -- done\n')
+		console.log('npm init -- done\n');
 
 		// installing dependencies
-		console.log('Installing deps -- it might take a few minutes..')
-		const devDeps = getDeps(packageJson.devDependencies)
-		const deps = getDeps(packageJson.dependencies)
+		console.log('Installing deps -- it might take a few minutes..');
+		const devDeps = getDeps(packageJson.devDependencies);
+		const deps = getDeps(packageJson.dependencies);
 		exec(
 			`cd ${process.argv[2]} && npm i -D ${devDeps} && npm i -S ${deps}`,
 			(npmErr, npmStdout, npmStderr) => {
 				if (npmErr) {
 					console.error(`it's always npm, ain't it?
-      ${npmErr}`)
-					return
+      ${npmErr}`);
+					return;
 				}
-				console.log(npmStdout)
-				console.log('Dependencies installed')
+				console.log(npmStdout);
+				console.log('Dependencies installed');
 
-				console.log('Copying additional files..')
+				console.log('Copying additional files..');
 				// copy additional source files
 				fs.copy(path.join(__dirname, '../src'), `${process.argv[2]}/src`)
 					.then(() =>
 						console.log(
 							`All done!\nYour project is now started into ${
 								process.argv[2]
-							} folder, refer to the README for the project structure.\nHappy Coding!`,
-						),
+							} folder, refer to the README for the project structure.\nHappy Coding!`
+						)
 					)
-					.catch(err => console.error(err))
-			},
-		)
-	},
-)
+					.catch(err => console.error(err));
+			}
+		);
+	}
+);
